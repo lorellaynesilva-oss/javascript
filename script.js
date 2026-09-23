23731f0f907ca8602aaa91f77a455e6887fb5e3b
lorellaynesilva-oss <lorellayne.silva@escola.pr.gov.br>
2026-09-23 19:59:33 +0000

    Alteração1

---
+++ script.js
@@ -0,0 +1,20 @@
+ function mostrarAlerta() {
+        alert ("Bom dia Turma do 2ºAno Exatas!")
+    }
+    function pedirNome () {
+        let nome = prompt ("informe o seu nome: ")
+        alert("Bom dia, " + nome)
+    }
+    funcion somarNumeros () {
+        let primeirovalor = Number (prompt("informe o primeiro número:"))
+        let segundovalor = Number (prompt("informe o segundo numero"))
+        let soma =primeirovalor + segundovalor
+
+        alert("A soma dos numeros é: " + soma)
+    }
+    funcion mudarFormatacao () {
+        let resultado = document.getElementById ("resultado")
+        let nome = prompt ("informe seu nome: ")
+        resultado
+    }
+
